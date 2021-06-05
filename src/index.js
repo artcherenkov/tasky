da2ee6adbe6ccb10e42ca140cb6bab92fe5bff95
Артем Черенков <art.cherenkov@gmail.com>
2021-06-05 19:27:47 +0700

    feat: добавить архитектуру хранилища

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "./store";
+import { createAPI } from "./services/api";
+
 import "./assets/css/style.css";
+
+const api = createAPI();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/store/app-store/app-store.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const appStore = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export { appStore };
